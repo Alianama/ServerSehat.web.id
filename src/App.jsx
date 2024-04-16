3afb737e40282983a4b6c30ded66075eb0555cb4
Alipurnama <Alipurnama69@gmail.com>
2024-04-16 14:16:53 +0700

    [Fix] add smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import Navbar from "././assets/pages/navbar/navbar";
 import Home from "././assets/pages/home/home";
 import About from "././assets/pages/about/about";
 import Layanan from "./assets/pages/layanan/layanan";
-
-function App() {
-  return (
-    <div className="flex flex-col ">
-      <Navbar />
-      <Home />
-      <About />
-      <Layanan />
-    </div>
-  );
-}
+const App = () => (
+  <div className="flex flex-col">
+    <Navbar />
+    <Home />
+    <About />
+    <Layanan />
+  </div>
+);
 
 export default App;
+
